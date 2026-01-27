d27ca6021b6cd3b0e7acb5420374777600ef588f
ialazzon <ialazzon@yahoo.com>
2026-01-27 16:27:52 +0400

    My first commit

---
+++ Spring_2026_Test/src/MainClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author issam
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("TEST TEST TEST");
+
+	}
+
+}
